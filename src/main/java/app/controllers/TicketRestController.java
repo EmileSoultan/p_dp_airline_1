d0ae6f285f9961ae6a4d64fa214a3c7222e2243f
Marat <hedarius1992@gmail.com>
2022-10-18 16:10:04 +0300

    Fixed RestController

--- src/main/java/app/controllers/TicketRestController.java
+++ src/main/java/app/controllers/TicketRestController.java
@@ -5,7 +5,6 @@ import app.services.TicketService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @ApiOperation("Ticket API")
 @Slf4j
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/ticket")
 public class TicketRestController {
     private final TicketService ticketService;
 
@@ -23,23 +22,18 @@ public class TicketRestController {
     }
 
     @ApiOperation(value = "Create new ticket", tags = "ticket-rest-controller")
-    @ApiResponses(value = {
-            @ApiResponse( code = 201, message = "Ticket created" )
-    })
-    @PostMapping("/ticket")
-    public ResponseEntity<Ticket> createTicket (@RequestBody Ticket ticket) {
+    @ApiResponse(code = 201, message = "Ticket created")
+    @PostMapping
+    public ResponseEntity<Ticket> createTicket(@RequestBody Ticket ticket) {
         log.info("methodName: createTicket - create new ticket");
         ticketService.saveTicket(ticket);
         return new ResponseEntity<>(ticket, HttpStatus.CREATED);
     }
 
     @ApiOperation(value = "Gets ticket by bookingNumber", tags = "ticket-rest-controller")
-    @ApiResponses(value = {
-            @ApiResponse( code = 200, message = "Found the ticket"),
-            @ApiResponse( code = 404, message = "Ticket not found")
-    })
+    @ApiResponse(code = 200, message = "Found the ticket")
     @GetMapping("/ticket")
-    public ResponseEntity<Ticket> showTicket (
+    public ResponseEntity<Ticket> showTicket(
             @RequestParam(value = "bookingNumber") @ApiParam("bookingNumber") String bookingNumber) {
         log.info("methodName: showTicketByBookingNumber - search ticket by bookingNumber");
         Ticket ticket = ticketService.findTicketByBookingNumber(bookingNumber);
@@ -49,10 +43,8 @@ public class TicketRestController {
     }
 
     @ApiOperation(value = "Update ticket", tags = "ticket-rest-controller")
-    @ApiResponses(value = {
-            @ApiResponse( code = 200, message = "Ticket has been updated")
-    })
-    @PatchMapping("/ticket/{id}")
+    @ApiResponse(code = 200, message = "Ticket has been updated")
+    @PatchMapping("/{id}")
     public ResponseEntity<Ticket> updateTicket(@RequestBody Ticket ticket) {
         log.info("methodName: updateTicket - update of current ticket");
         ticketService.updateTicket(ticket);
@@ -60,10 +52,8 @@ public class TicketRestController {
     }
 
     @ApiOperation(value = "Delete ticket", tags = "ticket-rest-controller")
-    @ApiResponses(value = {
-            @ApiResponse( code = 200, message = "Ticket has been removed")
-    })
-    @DeleteMapping("/ticket/{id}")
+    @ApiResponse(code = 200, message = "Ticket has been removed")
+    @DeleteMapping("/{id}")
     public ResponseEntity<Ticket> deleteTicket(@PathVariable @ApiParam("id") Long id) {
         log.info("methodName: deleteTicket - ticket of current destination");
         ticketService.deleteTicketById(id);
