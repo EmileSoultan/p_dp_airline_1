bac88b1d4466f89af362058b698e2fb508f30ff6
Marat <hedarius1992@gmail.com>
2022-10-18 16:15:08 +0300

    Fixed RestController, CodeStyle

--- src/main/java/app/services/TicketService.java
+++ src/main/java/app/services/TicketService.java
@@ -4,7 +4,10 @@ import app.entities.Ticket;
 
 public interface TicketService {
     Ticket findTicketByBookingNumber(String bookingNumber);
+
     void deleteTicketById(Long id);
+
     void saveTicket(Ticket ticket);
+
     void updateTicket(Ticket ticket);
 }
--- src/main/java/app/services/TicketServiceImp.java
+++ src/main/java/app/services/TicketServiceImp.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class TicketServiceImp implements TicketService{
+public class TicketServiceImp implements TicketService {
     private final TicketRepository ticketRepository;
 
     public TicketServiceImp(TicketRepository ticketRepository) {
