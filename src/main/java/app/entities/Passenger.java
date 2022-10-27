5cb869d087368ba9d695fe383fd4386565765143
Evgeny Sobolev <trustus@inbox.ru>
2022-10-27 08:33:19 +0000

    Closes #7

--- src/main/java/app/entities/Passenger.java
+++ src/main/java/app/entities/Passenger.java
@@ -2,10 +2,11 @@ package app.entities;
 
 import app.enums.Gender;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
 
 import javax.persistence.CascadeType;
@@ -13,43 +14,21 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
-import javax.persistence.Table;
-import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
-
-import java.time.LocalDate;
+import java.util.Date;
 
 @Entity
-@Table(name = "passenger")
-@Data
+@Setter
+@Getter
 @NoArgsConstructor
-@RequiredArgsConstructor
-public class Passenger {
-
-    @Id
-    @GeneratedValue
-    private Long id;
+@AllArgsConstructor
+public class Passenger extends User{
 
     /*TODO может прописать свой валидатор для email? @Email заточен на локальную почту (а у нас её не будет)
      * и принимает значения без домена почтового сервиса, ему @ достаточно и всё
      */
-    @NonNull
-    @NotEmpty(message = "The field cannot be empty")
-    @Email(message = "Incorrect email")
-    @Size(min = 1, max = 256, message = "Size email cannot be less than 1 and more than 256 characters")
-    @Column(unique = true)
-    private String email;
-
-    @NonNull
-    @NotEmpty(message = "The field cannot be empty")
-    @Size(min = 1, max = 64, message = "Size phone cannot be less than 1 and more than 64 characters")
-    @Column(name = "phone_number")
-    private String phoneNubmer;
 
     @NonNull
     @NotEmpty(message = "The field cannot be empty")
@@ -65,25 +44,30 @@ public class Passenger {
 
     @NonNull
     @NotEmpty(message = "The field cannot be empty")
+    @Column(name = "birth_date")
+    private Date birthDate;
+
+    @NonNull
+    @NotEmpty(message = "The field cannot be empty")
+    @Size(min = 1, max = 64, message = "Size phone cannot be less than 1 and more than 64 characters")
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @NonNull
+    @NotEmpty(message = "The field cannot be empty")
     @Size(min = 2, max = 128, message = "Size middle_name cannot be less than 2 and more than 128 characters")
     @Column(name = "middle_name")
     private String middleName;
 
     @NonNull
-    @Column(name = "date_of_birth", nullable = false)
-    private LocalDate dateOfBirth;
-
+    @NotEmpty(message = "The field cannot be empty")
     @ApiModelProperty(dataType = "string",
             value = "Return values \"мужской\" or \"женский\", accepts values see example",
             example = "\"male\", \"MALE\", \"m\", \"M\", \"female\", \"FEMALE\", \"f\", \"F\"")
-    @NonNull
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
     private Gender gender;
 
-    @NonNull
     @OneToOne(cascade = CascadeType.ALL)
-    @JoinColumn(name = "passenger_id")
     private Passport passport;
 
 }
---
+++ src/main/java/app/services/RoleServiceImpl.java
@@ -0,0 +1,29 @@
+package app.services;
+
+import app.entities.Role;
+import app.repositories.RoleRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@Transactional
+public class RoleServiceImpl implements RoleService{
+
+    private final RoleRepository roleRepository;
+
+    public RoleServiceImpl(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Role getRoleByName(String name) {
+        return roleRepository.findByName(name);
+    }
+
+    @Override
+    public void saveRole(Role role) {
+        roleRepository.save(role);
+    }
+}
