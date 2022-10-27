ba5c6df0941aeb73e3b969bc6ffe8a821a3e7501
Marat <hedarius1992@gmail.com>
2022-10-27 19:12:13 +0300

    Added s7 front page

--- src/main/java/app/controllers/MainController.java
+++ src/main/java/app/controllers/MainController.java
@@ -12,6 +12,6 @@ public class MainController {
     @GetMapping("/")
     public ModelAndView index() {
         log.info("index: logging example");
-        return new ModelAndView("index");
+        return new ModelAndView("s7");
     }
 }
