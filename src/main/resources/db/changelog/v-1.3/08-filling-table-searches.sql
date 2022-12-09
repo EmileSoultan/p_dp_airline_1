414f98bfc3e83ffbf47f22e7b18da2fa67dbba0c
Denis Pronin <de.pronin@gmail.com>
2022-12-09 13:15:51 +0600

    Revert "Closes #77 filling tables db"
    This reverts commit 0b81d167de749f3a78de589f561b453d8207d8d0.

--- src/main/resources/db/changelog/v-1.3/08-filling-table-searches.sql
+++
@@ -1,9 +0,0 @@
--- changeset aleksandr_netesov:1.3.8
-INSERT INTO searches(from_id, to_id, return_date, departure_date, number_of_passengers)
-VALUES ((SELECT destination.id FROM destination WHERE city_name = 'Москва'),
-        (SELECT destination.id FROM destination WHERE city_name = 'Омск'),
-        TO_DATE('2022/11/25', 'YYYY/MM/DD'), TO_DATE('2022/11/30', 'YYYY/MM/DD'), 2);
-INSERT INTO searches(from_id, to_id, return_date, departure_date, number_of_passengers)
-VALUES ((SELECT destination.id FROM destination WHERE city_name = 'Волгоград'),
-        (SELECT destination.id FROM destination WHERE city_name = 'Москва'),
-        TO_DATE('2022/11/27', 'YYYY/MM/DD'), TO_DATE('2022/11/30', 'YYYY/MM/DD'), 1);
