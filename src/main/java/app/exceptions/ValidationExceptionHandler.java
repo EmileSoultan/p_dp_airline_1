ce0b4126013472741bcaf58115b5177b40415c9d
Nikita Alekseenko <nukalekseenko@gmail.com>
2022-12-14 17:32:29 +0000

    Closes #65 exception handler unique constraint

--- src/main/java/app/exceptions/ValidationExceptionHandler.java
+++ src/main/java/app/exceptions/ValidationExceptionHandler.java
@@ -1,5 +1,6 @@
 package app.exceptions;
 
+import app.dto.SqlExceptionDto;
 import app.dto.ValidationExceptionDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,6 +8,8 @@ import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import java.sql.SQLException;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +20,12 @@ public class ValidationExceptionHandler {
     public ResponseEntity<List<ValidationExceptionDto>> handleException(BindException exception) {
         List<ValidationExceptionDto> validationExceptionDto = convertToErrorDtoList(exception);
         return new ResponseEntity<>(validationExceptionDto, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(SQLException.class)
+    public ResponseEntity<SqlExceptionDto> handleException(SQLException exception) {
+            SqlExceptionDto sqlExceptionDto = new SqlExceptionDto(exception.getMessage(), LocalDateTime.now());
+            return new ResponseEntity<>(sqlExceptionDto, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
     private List<ValidationExceptionDto> convertToErrorDtoList(BindException e) {
