34030f21fc698b05df5caa5754a9a7963c7e0920
Zahar Kolomiec <zaharkolomiec385@yandex.ru>
2023-02-14 15:44:27 +0000

    #122 closes added method for saving roles to entity, which save only UNIQUE roles and returns it

--- src/main/java/app/services/RoleServiceImpl.java
+++ src/main/java/app/services/RoleServiceImpl.java
@@ -1,11 +1,14 @@
 package app.services;
 
 import app.entities.user.Role;
+import app.entities.user.User;
 import app.repositories.RoleRepository;
 import app.services.interfaces.RoleService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @Transactional
@@ -23,6 +26,17 @@ public class RoleServiceImpl implements RoleService {
         return roleRepository.findByName(name);
     }
 
+    public Set<Role> saveRolesToUser(User user) {
+        Set<Role> userRoles = new HashSet<>();
+        user.getRoles().stream().forEach(a -> {
+            Role roleFromDb = getRoleByName(a.getName());
+            if (roleFromDb == null) {
+                throw new RuntimeException("role not found");
+            }
+            userRoles.add(roleFromDb);
+        });
+        return userRoles;
+    }
     @Override
     public void saveRole(Role role) {
         roleRepository.save(role);
