c857597aff9c1d2d345dc8d08c144a90ecf6eabc
Danil Tarasov <daniltarasov@yandex.ru>
2023-03-06 16:19:48 +0000

    #145 CLOSES seat rest controller dto

--- airline-project/src/main/java/app/repositories/SeatRepository.java
+++ airline-project/src/main/java/app/repositories/SeatRepository.java
@@ -2,9 +2,14 @@ package app.repositories;
 
 import app.entities.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
 
+    @Query(value = "select seat from Seat seat join seat.aircraft a where a.id = :id")
+    List<Seat> findByAircraftId(Long id);
 }
