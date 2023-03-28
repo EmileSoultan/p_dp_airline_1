0dbdcd68d256b2d90702301f571c5bfbe1e98332
Dmitrii Sergin <pvtlab@yandex.ru>
2023-03-28 20:18:55 +0000

    Closes #164 Create airline payments blank

--- airline-payments/src/main/java/app/PaymentsApplication.java
+++ airline-payments/src/main/java/app/PaymentsApplication.java
@@ -2,15 +2,12 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@RestController
 @EnableSwagger2
-public class AirlineApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(AirlineApplication.class, args);
-	}
-
+public class PaymentsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentsApplication.class, args);
+    }
 }
