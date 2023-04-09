3e8709da39e25f57ad46a6eb86023d8b4718c059
Yauheni Gud <goodevgeni@gmail.com>
2023-04-09 13:12:10 +0000

    #171 popravit-testovye-dannye-v-sql-skriptah

--- airline-project/src/main/java/app/AirlineApplication.java
+++ airline-project/src/main/java/app/AirlineApplication.java
@@ -11,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class AirlineApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AirlineApplication.class, args);
+
 	}
 
 }
