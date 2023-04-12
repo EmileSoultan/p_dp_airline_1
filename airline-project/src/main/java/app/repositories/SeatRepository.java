b4ca57ccd859dd8920d1210cb0f96866e8c969b7
MetelM <metelskymatvey@gmail.com>
2023-04-12 18:20:30 +0300

    #172 FlightSeatRestController and FlightSeatServiceImpl changes. addFlightSeatByFlightId is now independent. Tests changed.

--- airline-project/src/main/java/app/repositories/SeatRepository.java
+++ airline-project/src/main/java/app/repositories/SeatRepository.java
@@ -7,9 +7,15 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Set;
+
 @Repository
 public interface SeatRepository extends JpaRepository<Seat, Long> {
 
     @Query(value = "select seat from Seat seat join seat.aircraft a where a.id = :id")
     Page<Seat> findByAircraftId(Long id, Pageable pageable);
+
+    @Query(value = "select seat from Seat seat join seat.aircraft a where a.id = :id")
+    Set<Seat> findByAircraftId(Long id);
+
 }
