cadd35ea45fa0c4cc0549f5c83c2a1c146615127
Rustam Fatkhullin <qwerus99@yandex.ru>
2023-06-19 22:06:27 +0300

    225 noSecurity config csrf disable

--- airline-project/src/main/java/app/config/security/NoSecurityConfig.java
+++ airline-project/src/main/java/app/config/security/NoSecurityConfig.java
@@ -17,6 +17,7 @@ public class NoSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception{
         http
+                .csrf().disable()
                 .authorizeRequests()
                 .anyRequest()
                 .permitAll();
